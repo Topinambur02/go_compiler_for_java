67c55107bbac217d58e4faf37f49f9f6e908f052
Topinambur02 <alext0408@yandex.ru>
2026-05-13 15:21:29 +0300

    fix: add int a; in test program

--- test.java
+++ test.java
@@ -7,6 +7,7 @@ public class Test {
             for (int j = 0; j < array.length - i - 1; j++) {
                 if (array[j] > array[j + 1]) {
                     // Swap arr[j] and arr[j+1]
+                    int a;
                     int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
